fae1c89befcadf74dfeaf2e3526780b9f58f3c4c
Yan Carvalho <yan.carvalho.profissional@gmail.com>
2025-08-02 02:16:49 +0000

    DESAFIoXADREZZ

--- xadrez.c
+++ xadrez.c
@@ -23,6 +23,42 @@ int main() {
         printf("Esquerda\n");
         k++;
     } while (k < 8);
+    
+    // CAVALO: CIMA CIMA DIREITA OU CIMA CIMA ESQUERDA
+    
+    char lado;
+
+    printf("\nMovimentos do cavalo:\n");
+
+    printf("\nPara qual lado você deseja mover o cavalo? (d = Direita, e = esquerda): \n");
+
+    scanf("%c" , &lado);
+
+    if(lado == 'd' || lado == 'D'){
+        printf("Você escolheu mover para a direita.\n");
+        // CAVALO: 2 para cima 1 para direita
+    
+    for (int c = 0; c < 2; c++) {
+        printf("Cima\n");
+    }
+    printf("Direita\n");
+
+    } else if (lado == 'e' || lado == 'E'){
+        printf("Você escolheu mover para a esquerda.\n");
+        // CAVALO: 2 para cima 1 para a Esquerda
+    
+    for (int c = 0; c < 2; c++) {
+        printf("Cima\n");
+    }
+    printf("Esquerda\n");
+
+    }else {
+        printf("Opção inválida. \n");
+    }
+     
 
     return 0;
+
+
+    
 }
